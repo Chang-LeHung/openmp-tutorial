5611130ffcb208563d6e669b9cdbf29b3c1a70ed
Chang-LeHung <799207539@qq.com>
2023-02-14 20:40:57 +0800

    update openmp for construct guided

--- src/principle/principle_guided.c
+++ src/principle/principle_guided.c
@@ -5,8 +5,8 @@
 
 int main()
 {
-#pragma omp parallel for num_threads(4) default(none) schedule(guided, 10)
-  for(int i = 0; i < 100; i += 5)
+#pragma omp parallel for num_threads(4) default(none) schedule(guided, 4)
+  for(int i = 0; i < 100; i += 1)
   {
     printf("i = %d tid = %d\n", i, omp_get_thread_num());
   }
